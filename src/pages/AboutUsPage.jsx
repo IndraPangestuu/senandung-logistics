70877cb623b5d64a98c8198b3f17b6ca948b8e50
indrapangestuu <matiasindrapangestu@gmail.com>
2025-06-11 06:43:00 +0700

    Update Hero Section

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import HeroSection from '../components/HeroSection';
+import heroImage from '../assets/hero2.jpg';
 import Footer from '../components/Footer'
 import Section1 from '../content/AboutUs/section1';
 // import Section2 from '../content/AboutUs/section2';
@@ -9,6 +11,13 @@ function AboutUs() {
   return (
     <>
       <Navbar />
+      <HeroSection
+        imageSrc={heroImage}
+        title="About"
+        subTitle="Home > About"
+        height='h-[70vh]'
+        textSize='text-[2.5vw] sm:text-sm'
+      />
       <div className="min-h-screen flex flex-col px-20 lg:px-20">
         <main className="flex-grow">
           <Section1 />
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,8 @@ import Footer from '../components/Footer'
 import Section1 from '../content/Home/section1';
 import Section2 from '../content/Home/section2';
 import Section3 from '../content/Home/section3';
+import heroImage from '../assets/hero1.jpg';
+import HeroSection from '../components/HeroSection';
 
 function HomePage() {
   return (
@@ -11,6 +13,11 @@ function HomePage() {
       <Navbar />
       <div className="min-h-screen flex flex-col">
         <main className="flex-grow">
+          <HeroSection
+            imageSrc={heroImage}
+            title="Percayakan Export Import"
+            subTitle="Anda pada Ahlinya"
+          />
           <Section1 />
           <Section2 />
           <Section3 />
